7afd8977afae9962db8cabf7d776a6e3a6af3436
SnoozeZ <zhangweiqi357@gmail.com>
2015-11-30 17:05:57 -0800

    add BasicAnalysis.h (Why disappeared?)

---
+++ include/BasicAnalysis.h
@@ -0,0 +1,34 @@
+#ifndef BASIC_ANALYSIS
+#define BASIC_ANALYSIS
+
+#include "CFGUtils.h"
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/InstIterator.h"
+#include <vector>
+#include <queue>
+
+using namespace llvm;
+using namespace std;
+
+class BasicAnalysis{
+private:
+  vector<CFGNode *> CFGNodes;
+  vector<CFGEdge *> CFGEdges;
+  CFGNode *CFGHead;
+
+public:
+  BasicAnalysis();
+  BasicAnalysis(Function &F);
+  void createCFG(Function &F);
+  void runWorkList();
+
+  virtual LatticeNode *runFlowFunc(LatticeNode *in, CFGNode *curNode);
+  virtual LatticeNode *latticeNodeInit();// Can write in the createCFG func or not?
+  virtual ~BasicAnalysis();
+
+};
+#endif
